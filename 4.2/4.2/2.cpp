4ec0210af9b0c31eddce3dfc8a5ab71ff66a5582
Ekaterina Petrova <penspb@gmail.com>
2016-12-25 21:12:25 +0300

    Corrected

--- 4.2/4.2/2.cpp
+++ 4.2/4.2/2.cpp
@@ -1,18 +1,43 @@
 #include <iostream>
-#include "binary.h"
+#include "qSort.h"
 #include <fstream>
 
 using namespace std;
+
+int searching(int array[], int length)
+{
+	qSort(array, 0, length - 1);
+
+	int quantity = 1;
+	int more = 1;
+	int element = array[0];
+
+	for (int i = 1; i < length; i++)
+	{
+		if (array[i] == array[i - 1])
+		{
+			quantity++;
+		}
+		else
+		{
+			if (quantity > more)
+			{
+				more = quantity;
+				element = array[i - 1];
+			}
+			quantity = 1;
+		}
+	}
+
+	return element;
+}
 
 bool test()
 {
 	const int length = 9;
 	int array[length] = {1, 2, 3, 2, 9, 9, 2, 1, 2};
-	if (searching(array, length) == 2)
-	{
-		return true;
-	}
-	return false;
+	
+	return searching(array, length) == 2;
 }
 
 int main()
--- 4.2/4.2/binary.h
+++
@@ -1,9 +0,0 @@
-
-//быстрая сортировка
-void qSort(int array[], int left, int right);
-
-//поиск опорного элемента
-void partition(int array[], int &left, int &right);
-
-//поиск искомого элемента
-int searching(int array[], int length);
