b42d19e8c5bd3a081b3bb871d11c0af0734ec05d
Ekaterina Petrova <penspb@gmail.com>
2016-12-26 21:10:26 +0300

    6.1

---
+++ 6.1/6.1/stact.cpp
@@ -0,0 +1,46 @@
+#include "stack.h"
+#include <iostream>
+
+using namespace std;
+
+struct Stack
+{
+	int value;
+	Stack *next;
+};
+
+void deleteStack(Stack *&head)
+{
+	while (head != nullptr)
+	{
+		Stack *oldHead = head;
+		head = head->next;
+	}
+}
+
+void push(Stack *&head, int value)
+{
+	head = new Stack{ value, head };
+}
+
+bool isEmpty(Stack *head)
+{
+	return head == nullptr;
+}
+
+int pop(Stack *&head)
+{
+	if (!isEmpty(head))
+	{
+		Stack *oldHead = head;
+		head = head->next;
+
+		int value = oldHead->value;
+		delete oldHead;
+		return value;
+	}
+	else
+	{
+		return 10;
+	}
+}
